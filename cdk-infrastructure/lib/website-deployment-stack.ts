956ad81d476e3d673b6fbeff8297d7ac132818b8
pabcrudel <pabcrudel@gmail.com>
2023-07-27 18:18:57 +0200

    Created s3 bucket to host the website files

--- cdk-infrastructure/lib/website-deployment-stack.ts
+++ cdk-infrastructure/lib/website-deployment-stack.ts
@@ -6,6 +6,19 @@ export class WebsiteDeploymentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    
+    /** The s3 bucket where the website will be hosted */
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      publicReadAccess: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      accessControl: s3.BucketAccessControl.PRIVATE,
+      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+    });
+    /*
+    Not turned on the static website feature of the S3 service 
+    as it does not offer the caching and distribution benefits that the CloudFront service does.
+    */
   }
 }
