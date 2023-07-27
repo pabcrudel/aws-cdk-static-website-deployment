ed9bc741ebdf0bfa12bb7a0bf4b70dafddfacd3d
pabcrudel <pabcrudel@gmail.com>
2023-07-27 23:29:37 +0200

    Deleted useless cloud formation outputs

--- cdk-infrastructure/lib/website-deployment-stack.ts
+++ cdk-infrastructure/lib/website-deployment-stack.ts
@@ -25,12 +25,6 @@ export class WebsiteDeploymentStack extends cdk.Stack {
       */
     });
 
-    // Displays the S3 Bucket ARN and name on CloudFormation output
-    new cdk.CfnOutput(this, 'S3BucketDetails', {
-      value: `Bucket name:\n${websiteBucket.bucketName}\nBucket ARN:\n${websiteBucket.bucketArn}`,
-      description: 'Name and ARN of the S3 bucket hosting the website',
-    });    
-
     /** Creating A CloudFront Origin Access Identity (OAI) user */
     const cloudfrontOAI = new cloudfront.OriginAccessIdentity(
       this, 'CloudFrontOriginAccessIdentity'
@@ -42,15 +36,9 @@ export class WebsiteDeploymentStack extends cdk.Stack {
       resources: [websiteBucket.arnForObjects('*')],
       principals: [new iam.CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)],
     });
-    
+
     // Adding OAI user to the website bucket
     websiteBucket.addToResourcePolicy(policyStatement);
-
-    // Displays the policy for CloudFront OAI to access the S3 bucket on CloudFormation output
-    new cdk.CfnOutput(this, 'AccessPolicyDetails', {
-      value: JSON.stringify(policyStatement.toStatementJson(), null, 2),
-      description: 'Policy for CloudFront Origin Access Identity to access the S3 bucket',
-    });
 
     /** Cloudfront Response Headers Policy */
     const responseHeaderPolicy = new cloudfront.ResponseHeadersPolicy(this, 'SecurityHeadersResponseHeaderPolicy', {
@@ -85,12 +73,6 @@ export class WebsiteDeploymentStack extends cdk.Stack {
       }
     });
 
-    // Displays ARN of the CloudFront response headers policy on CloudFormation output
-    new cdk.CfnOutput(this, 'CloudFrontResponseHeadersPolicy', {
-      value: responseHeaderPolicy.node.addr,
-      description: 'ARN of the CloudFront response headers policy',
-    });
-
     /** Binding S3 bucket, OAI user and Response Headers Policy to the Cloudfront distribution */
     const cloudfrontDistribution = new cloudfront.Distribution(this, 'CloudFrontDistribution', {
       defaultRootObject: 'index.html',
@@ -113,18 +95,18 @@ export class WebsiteDeploymentStack extends cdk.Stack {
       ]
     });
 
-    // Displays CloudFront domain name on CloudFormation output
-    new cdk.CfnOutput(this, 'CloudFrontDomainName', {
-      value: cloudfrontDistribution.domainName,
-      description: 'Domain name of the CloudFront distribution',
-    });
-
     /** Deploying the built files from the frontend to the s3 hosting the website */
     new s3deploy.BucketDeployment(this, 'WebsiteBucketDeployment', {
       sources: [s3deploy.Source.asset('../www'),],
       prune: false,
       destinationBucket: websiteBucket,
       distribution: cloudfrontDistribution,
+    });
+
+    // Displays CloudFront domain name on CloudFormation output
+    new cdk.CfnOutput(this, 'CloudFrontDomainName', {
+      value: cloudfrontDistribution.domainName,
+      description: 'Domain name of the CloudFront distribution',
     });
   }
 }
