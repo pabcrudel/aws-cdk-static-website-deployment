5ecdec677d5043ef75c44e19ad7e04cca3f4db50
pabcrudel <pabcrudel@gmail.com>
2023-07-27 18:23:46 +0200

    Added OAI user to grant cloudfront read access

--- cdk-infrastructure/lib/website-deployment-stack.ts
+++ cdk-infrastructure/lib/website-deployment-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class WebsiteDeploymentStack extends cdk.Stack {
@@ -20,5 +22,18 @@ export class WebsiteDeploymentStack extends cdk.Stack {
     Not turned on the static website feature of the S3 service 
     as it does not offer the caching and distribution benefits that the CloudFront service does.
     */
+
+    /** A CloudFront Origin Access Identity (OAI) user is granted read access to the S3 bucket objects through the bucket resource policy to enable user access via CloudFront. */
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(
+      this, 'CloudFrontOriginAccessIdentity'
+    );
+
+    // Adding OAI user to the website bucket
+    websiteBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [websiteBucket.arnForObjects('*')],
+      principals: [new iam.CanonicalUserPrincipal(
+        cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)],
+    }));
   }
 }
